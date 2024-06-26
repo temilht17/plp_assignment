ec81dc79570160d320a14a31d0f3e0ef65fab1cc
temilht17 <temitopelht@gmail.com>
2024-06-26 22:55:03 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Sample JavaScript code
+console.log("Hello, JavaScript!");
+
+// Example function
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+// Call the function
+greet("World");
